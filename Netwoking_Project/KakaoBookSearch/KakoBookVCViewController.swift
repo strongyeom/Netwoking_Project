e1d9b7a00dfa8dfe751b6b95ddf795aa4169d77a
yeomsungpil <present7256@gmail.com>
2023-08-09 17:59:14 +0900

    실시간으로 offset 값 계산해서 reload 하기 [targetContentOffset.pointee.y]

--- Netwoking_Project/KakaoBookSearch/KakoBookVCViewController.swift
+++ Netwoking_Project/KakaoBookSearch/KakoBookVCViewController.swift
@@ -24,6 +24,9 @@ class KakoBookVCViewController: UIViewController {
     
     var pageableCount: Int = 0
     
+   
+    let indicator = UIActivityIndicatorView(style: .large)
+    
     @IBOutlet var kakaoCollectionView: UICollectionView!
     
     override func viewDidLoad() {
@@ -34,13 +37,17 @@ class KakoBookVCViewController: UIViewController {
         settingSearchBar()
         callRequest(page: page, isEnd: isEnd)
         searchBookCount.text = "도서 검색 \(pageableCount)개"
+       
+        indicator.tintColor = .red
+        self.kakaoCollectionView.addSubview(indicator)
+        
     }
     
     func settingInitial() {
        
         kakaoCollectionView.delegate = self
         kakaoCollectionView.dataSource = self
-        kakaoCollectionView.prefetchDataSource = self
+       // kakaoCollectionView.prefetchDataSource = self
         let nib = UINib(nibName: KakaoCollectionViewCell.identifier, bundle: nil)
         
         kakaoCollectionView.register(nib, forCellWithReuseIdentifier:  KakaoCollectionViewCell.identifier)
@@ -65,7 +72,7 @@ class KakoBookVCViewController: UIViewController {
     
     func callRequest(text: String = "성공", page: Int, isEnd: Bool) {
         //self.bookList.removeAll()
-        
+        print("바닥일때 추가됌")
         let textASCII = "\(text)".addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!
         let url = "https://dapi.kakao.com/v3/search/book?query=\(textASCII)&size=15&page=\(page)&is_end=\(isEnd)"
         let header: HTTPHeaders = ["Authorization" : APIKey.KakoKey]
@@ -76,10 +83,10 @@ class KakoBookVCViewController: UIViewController {
             switch response.result {
             case .success(let value):
                 let json = JSON(value)
-                print("json",json)
+               // print("json",json)
                 
                 self.pageableCount = json["meta"]["pageable_count"].intValue
-                print("callRequest - pageableCount",self.pageableCount)
+                // print("callRequest - pageableCount",self.pageableCount)
               
                 for item in json["documents"].arrayValue {
                     let imageTitle = item["thumbnail"].stringValue
@@ -87,7 +94,6 @@ class KakoBookVCViewController: UIViewController {
                    
                     let data = KakoBook(imageUrl: imageTitle, bookTitle: bookname)
                    
-                    
                     self.bookList.append(data)
                 }
                 
@@ -104,12 +110,38 @@ class KakoBookVCViewController: UIViewController {
 
 // MARK: - UICollectionViewDelegate
 extension KakoBookVCViewController : UICollectionViewDelegate {
+    // 스크롤 하는 중일때 실시간으로 반영하는 방법은 없을까?
+    func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
+        
+        let contentSize = scrollView.contentSize.height
+        let scrollViewHeight = scrollView.frame.height
+        
+        //contentOffSet 도 스크롤한 길이를 나타내지만 실시간으로 반영해주진 않는다.
+        let offset = scrollView.contentOffset.y
+        
+        // targetContentOffset.pointee.y: 사용자가 스크롤하면 실시간으로 값을 나타낼 수 있음
+        let targetPointOfy = targetContentOffset.pointee.y
+        
+        let doneScrollOffSet = contentSize - scrollViewHeight
+        print("contentSize",contentSize)
+        print("offset ",offset)
+        print("scrollViewHeight",scrollViewHeight)
+        print("doneScrollOffSet",doneScrollOffSet)
+        print("targetContentOffset.y",targetPointOfy)
     
+        if targetPointOfy + 40 >= doneScrollOffSet {
+            print("바닥이다")
+            page += 1
+            callRequest(page: page, isEnd: isEnd)
+        }
+    }
 }
 
 
 // MARK: - UICollectionViewDataSource
 extension KakoBookVCViewController: UICollectionViewDataSource {
+    
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return bookList.count
     }
@@ -130,6 +162,7 @@ extension KakoBookVCViewController: UICollectionViewDataSource {
 extension KakoBookVCViewController: UICollectionViewDataSourcePrefetching {
     
     func collectionView(_ collectionView: UICollectionView, prefetchItemsAt indexPaths: [IndexPath]) {
+        print("UICollectionViewDataSourcePrefetching")
         // 응답 메세지로 page에 대한 값이 정해져 있다면 조건을 추가해준다.
         // 또한 마지막 페이지가 나오면 더이상 page를 증가시키지 않는다.
         for indexPath in indexPaths {
